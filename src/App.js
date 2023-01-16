ec99b8a413f8f2479bd438781217196b0b2e04f1
JorgeGReyes <JorgeGReyes018@yahoo.com>
2023-01-16 14:33:59 -0500

    adding components to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
 import "./App.css";
 
+import Form from "./components/form/form";
+import List from "./components/list/list";
+
 function App() {
+  const [inputText, setInputText] = React.useState({});
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Task List</h1>
       </header>
+      <div>
+        <Form />
+      </div>
+      <div>
+        <List />
+      </div>
     </div>
   );
 }
