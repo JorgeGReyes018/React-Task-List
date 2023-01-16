e15e9fff121f3c3e8c30934e8063454f6a288bf2
JorgeGReyes <JorgeGReyes018@yahoo.com>
2023-01-16 18:14:35 -0500

    doneHandler

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -4,10 +4,25 @@ export default function Task({ text, task, setTasks, tasks }) {
   const delHandler = () => {
     setTasks(tasks.filter((el) => el.id !== task.id));
   };
+  const doneHandler = () => {
+    setTasks(
+      tasks.map((item) => {
+        if (item.id === task.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
   return (
     <div className="task">
       <li className="taskItem">{text}</li>
-      <button className="doneBtn">Done</button>
+      <button onClick={doneHandler} className="doneBtn">
+        Done
+      </button>
       <button onClick={delHandler} className="delBtn">
         Delete
       </button>
